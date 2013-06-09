9cd38efc3102ac06d03f42ceb045bccda22dd041
Sviridov Alexander <sviridov.vmi@gmail.com>
2013-06-10 01:01:47 +0600

    Added downloads_status_widget modeline hook

--- initModeline.js
+++ initModeline.js
@@ -1,5 +1,6 @@
 
 add_hook("mode_line_hook", mode_line_adder(buffer_count_widget), true);
 add_hook("mode_line_hook", mode_line_adder(loading_count_widget), true);
+add_hook("mode_line_hook", mode_line_adder(downloads_status_widget));
 
 provide ("initModeline");
