ffd5ef6b4823c05e2514859e2397c1a974c71d10
Alexander Sviridov <sviridov.vmi@gmail.com>
2013-06-09 18:15:20 +0600

    Modified addToLoadPath

--- init.js
+++ init.js
@@ -1,6 +1,6 @@
 
 function addToLoadPath (path = "") {
-    load_paths.unshift("file://" + get_home_directory().path + "/.conkeror.d/");
+    load_paths.unshift("file://" + get_home_directory().path + "/.conkeror.d/" + path);
 }
 
 addToLoadPath ();
