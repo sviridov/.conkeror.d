767a9a6d161399fe62a4961928319b3fc1827b26
Sviridov Alexander <sviridov.vmi@gmail.com>
2013-06-11 13:09:59 +0600

    Added restoreKilledBufferURL

--- initBuffers.js
+++ initBuffers.js
@@ -31,4 +31,39 @@ define_key (default_global_keymap, "C-x b", "switch-to-recent-buffer");
 
 
 
+var killBufferOriginal = killBufferOriginal || kill_buffer;
+
+var killedBufferURLs = [];
+
+kill_buffer = function (buffer, force) {
+    if (buffer.display_uri_string) {
+        killedBufferURLs.push (buffer.display_uri_string);
+    }
+    killBufferOriginal (buffer,force);
+};
+
+function restoreKilledBufferURL (I) {
+    if (killedBufferURLs.length !== 0) {
+        var url = yield I.minibuffer.read (
+            $prompt = "Restore killed url:",
+            $completer = all_word_completer ($completions = killedBufferURLs),
+            $default_completion = killedBufferURLs [killedBufferURLs.length - 1],
+            $auto_complete = "url",
+            $auto_complete_initial = true,
+            $auto_complete_delay = 0,
+            $match_required);
+        load_url_in_new_buffer (url);
+    } else {
+        I.window.minibuffer.message ("No killed buffer urls");
+    }
+}
+
+interactive("restore-killed-buffer-url",
+            "Loads url from a previously killed buffer",
+            restoreKilledBufferURL);
+
+define_key (default_global_keymap, "C-x C-b", "restore-killed-buffer-url");
+
+
+
 provide ("initBuffers");
