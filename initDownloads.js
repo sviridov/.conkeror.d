54f5b345dcc2bbf3d51b28a3d2ff4ff840c2dd6f
Sviridov Alexander <sviridov.vmi@gmail.com>
2013-06-11 17:02:46 +0600

    Conkeror remembers the last save directory for downloads

--- initDownloads.js
+++ initDownloads.js
@@ -1,4 +1,16 @@
 
 download_buffer_automatic_open_target = OPEN_NEW_BUFFER;
 
+var savePath = get_home_directory ().path  + "/Загрузки";
+
+function updateSavePath (info) {
+    savePath = info.target_file.parent.path;
+}
+
+add_hook ("download_added_hook", updateSavePath);
+
+suggest_save_path_from_file_name = function (filename, buffer) {
+    return savePath + "/" + filename;
+};
+
 provide ("initDownloads");
